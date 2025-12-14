4a18e01dfb511cd5788017a616615a1711f1b332
LazyDev <lamhiep2601@gmail.com>
2025-12-14 16:23:11 +0700

    feat: show history transaction, filter filter transaction

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,7 +2,7 @@ const loginRoute = require("./login.route");
 const registerRoute = require("./register.route");
 const chatRoute = require("./chat.route");
 // const statisticRoute = require("./statistic.route");
-// const transactionRoute = require("./transactions.route");
+const transactionRoute = require("./transactions.route");
 
 module.exports.index = (app) => {
   app.get("/login", loginRoute);
@@ -15,12 +15,12 @@ module.exports.index = (app) => {
   app.get("/chat", chatRoute);
   app.post("/chat", chatRoute);
 
-  //   app.get("/statistic", statisticRoute);
-  //   app.get("/statistic/expense-data", statisticRoute);
+  // app.get("/statistic", statisticRoute);
+  // app.get("/statistic/expense-data", statisticRoute);
 
-  //   app.get("/transaction", transactionRoute);
-  //   app.get("/transaction-data", transactionRoute);
-  //   app.get("/transaction-filter", transactionRoute);
-  //   app.delete("/transaction-data/:id", transactionRoute);
-  //   app.put("/transaction-data/:id", transactionRoute);
+  app.get("/transaction", transactionRoute);
+  app.get("/transaction-data", transactionRoute);
+  app.get("/transaction-filter", transactionRoute);
+  app.delete("/transaction-data/:id", transactionRoute);
+  app.put("/transaction-data/:id", transactionRoute);
 };
